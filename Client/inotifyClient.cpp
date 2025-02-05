23cf1b698569f627b95825918b046d83e681d920
Andrei <andrei.koenich@gmail.com>
2025-02-05 00:33:16 -0300

    Problema envolvendo multiplas notificacoes do INotify para um mesmo evento foi resolvido

--- Client/inotifyClient.cpp
+++ Client/inotifyClient.cpp
@@ -13,13 +13,9 @@ using namespace std;
 #define BUF_LEN     (1024 * (EVENT_SIZE + NAME_MAX + 1))
 
 bool isTemporaryFile(const string filename) {
-
-    //cout << "Nome do arquivo sendo monitorado: " << filename << endl;
-    if (filename.front() == '~' || filename.front() == '.')
-        return true;
-    else
-        return false;
+    return filename.front() == '~' || filename.front() == '.';
 }
+
 
 void* monitorClientDirectory (void* usernamePtr) {
 
@@ -44,7 +40,7 @@ void* monitorClientDirectory (void* usernamePtr) {
         return NULL;
     }
 
-    int watch_descriptor = inotify_add_watch(inotify_fd, clientDirectory, IN_MOVED_FROM | IN_MOVED_TO | IN_CLOSE_WRITE);
+    int watch_descriptor = inotify_add_watch(inotify_fd, clientDirectory, IN_MOVED_FROM | IN_MOVED_TO | IN_CLOSE_WRITE | IN_CREATE);
     
     if (watch_descriptor < 0) {
         cerr << "Erro ao adicionar watch ao diretório." << endl;
@@ -55,6 +51,9 @@ void* monitorClientDirectory (void* usernamePtr) {
     char buffer[BUF_LEN];
     cout << "Monitorando o diretório: " << clientDirectory << endl;
 
+    string filename, lastModifiedFile;
+    bool modificationFlag = false;
+
     while (true) {
         int length = read(inotify_fd, buffer, BUF_LEN);
         if (length < 0) {
@@ -63,20 +62,35 @@ void* monitorClientDirectory (void* usernamePtr) {
         }
 
         for (int i = 0; i < length;) {
-            struct inotify_event *event = (struct inotify_event *) &buffer[i];
-            if (event->len) {
-                string filename = event->name;
-                if (!isTemporaryFile(filename))
-                {
-                    if (event->mask & IN_CLOSE_WRITE)
-                        cout << "Arquivo " << filename << " foi modificado." << endl;
-                    else if (event->mask & IN_MOVED_FROM)
-                        cout << "Arquivo " << filename << " foi movido para fora do diretorio." << endl;
-                    else if (event->mask & IN_MOVED_TO)
-                        cout << "Arquivo " << filename << " foi movido para dentro do diretorio." << endl;
+                    struct inotify_event *event = (struct inotify_event *) &buffer[i];
+                    if (event->len) {
+                        string filename = event->name;
+                        
+                        if (!isTemporaryFile(filename))
+                        {   
+                            if (event->mask & IN_MOVED_TO || event->mask & IN_CREATE) {
+
+                                if (lastModifiedFile == filename) 
+                                    modificationFlag = true;
+                                else
+                                    cout << "Arquivo " << filename << " foi movido para dentro do diretorio." << endl;
+                            }
+                        
+                            else if (event->mask & IN_CLOSE_WRITE) {
+                                lastModifiedFile = filename;
+
+                                if (modificationFlag == true) {
+                                    cout << "Arquivo " << filename << " foi modificado." << endl;
+                                    modificationFlag = false;
+                                }   
+                            }
+
+                            else if (event->mask & IN_MOVED_FROM)
+                                    cout << "Arquivo " << filename << " foi movido para fora do diretorio." << endl;
+                        
+                    }
+                    i += EVENT_SIZE + event->len;
                 }
-            }
-            i += EVENT_SIZE + event->len;
         }
     }
 
